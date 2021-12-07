226fbea3519a91cdc76820633b7406305e5f63df
britttheuser <britt.zhang@gmail.com>
2021-12-07 09:59:35 -0800

    updated readme and button features for continent

---
+++ GapminderApp/README.md
@@ -0,0 +1,29 @@
+Assignment B4 Shiny App
+================
+
+# An application to explore the gapminder dataset
+
+This is an assignment B4 completed for UBC Stat 545B course. This app is
+not an extension of the BCL app submitted to assignment B3; this has
+been started from scratch.
+
+The application explores the gapminder dataset contained in the
+gapminder package
+(<https://cran.r-project.org/web/packages/gapminder/README.html>)
+
+The app can be found at
+<https://britttheuser.shinyapps.io/GapminderApp/>
+
+## Here are the functionalities of the current app:
+
+1.  The application theme is set to be darkly
+2.  User can choose the continent then the country they wish to see the
+    data of, the country drop down menu reacts to the continent choice
+3.  A slider is available to restrict the year range of output and a
+    drop down menu for the variable to be compared over the years
+4.  A plot and a table will be generated from the above selections (2
+    & 3) in separate tabs
+5.  Radio buttons are also available to see overviews by continent (mean
+    of all countries within the chosen continent)
+6.  A tab to for users to type in the country name to view the
+    information for a specific country
--- GapminderApp/app.R
+++ GapminderApp/app.R
@@ -5,7 +5,7 @@ library(stringr)
 library(gapminder)
 #library(rnaturalearthhires)
 options(shiny.autoreload = TRUE)
-output_variables <- select(gapminder, c(year, lifeExp, pop, gdpPercap))
+output_variables <- select(gapminder, c(lifeExp, pop, gdpPercap))
 #reference to dynamic select menu from  https://shiny.oxshef.io/tutorials_controls-dependent-on-data.html, https://github.com/OxShef/oxshef_shiny/tree/master/tutorial-apps/controls-dependent-on-data
 
 
@@ -14,17 +14,20 @@ ui <- fluidPage(
     titlePanel("Socioeconomic data for 142 countries"),
     strong("This is an app that helps you explore the population and GDP of countries around the world"),
     br(),
+    "You can choose a country and a varaible to see its changes thorughout the years",
+    br(),
     br(),
     fluidPage(theme = shinytheme("darkly")),
     sidebarLayout(
-        sidebarPanel("You can input here: ",
-                     "The pulldown menu below contains only those continents included in the gapminder dataset.",
+        sidebarPanel(tags$b("You can input here: "),
+                     hr(),
+                     "First, choose a continent",
                      # uiOutput("selected_continent_UI"),
                      selectInput("selected_continent",
                                  "Selected continent:",
                                  choices = ""),
                      br(),
-                     "The pulldown menu below is populated with countries included in the gapminder dataset which are members of the continent chosen above.",
+                     "Select a country that is a member of the continent chosen above.",
                      # uiOutput("selected_country_UI"),
                      selectInput("selected_country",
                                  "Selected country:",
@@ -33,14 +36,15 @@ ui <- fluidPage(
                      sliderInput("my_slider", "Select a year range", min = 1952, max = 2007, step = 10, value = c(1952, 2007), ticks = TRUE),
                      actionButton("reset", "Reset"),
                      hr(),
-                     varSelectInput("my_variable", "Select the data you want to see", data = gapminder, selected = "gdpPercap", multiple = FALSE),
+                     varSelectInput("my_variable", "Select the data you want to see", data = output_variables, selected = "gdpPercap", multiple = FALSE),
                      hr(),
-                     selectInput("my_select", "Select a continent you wish to see the overview of ", choices = unique(gapminder$continent)),
+                     radioButtons("my_select", "Select a continent you wish to see the overview of ", choices = unique(gapminder$continent)),
 
         ),
 
-        mainPanel("Your result will be displayed here: ",
+        mainPanel(tags$b("Your results are displayed here: "),
                   br(),
+                  hr(),
                   tabsetPanel(
                       tabPanel("Plot",
                                plotOutput("my_plot")),
@@ -101,9 +105,11 @@ server <- function(input, output, session) {
     output$my_plot<-renderPlot(
         filtered() %>%
             ggplot(aes(year, !!input$my_variable)) +
+            geom_point(alpha = 0.2) +
             geom_line(color="#34BD94") +
             xlab("Year") +
-            ylab(input$my_variable)
+            ylab(input$my_variable) +
+            scale_y_log10()
             #theme_ipsum()
     )
 
